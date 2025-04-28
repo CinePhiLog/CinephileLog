b6393e2669d274caeb35360d6c101cde06a31d0d
ehdwo13 <ehdwo13@gmail.com>
2025-04-28 14:10:45 +0900

    add init chatting function logic

---
+++ src/main/java/com/CinephileLog/domain/Message.java
@@ -0,0 +1,24 @@
+package com.CinephileLog.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+
+    private Long roomId;
+    private Long userId;
+    private Timestamp sendTime;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+}
+
