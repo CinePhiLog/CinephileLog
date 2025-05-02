39d2e59e301a2b857c3af29cf5cfbcccde22cbf0
elini_ng <huangenni@yahoo.com>
2025-05-02 16:15:26 +0900

    Recover UserController, UserRepository, UserService, WebSecurityConfig files changes after merge code

--- src/main/java/com/CinephileLog/service/UserService.java
+++ src/main/java/com/CinephileLog/service/UserService.java
@@ -7,7 +7,6 @@ import com.CinephileLog.repository.GradeRepository;
 import com.CinephileLog.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -47,22 +46,8 @@ public class UserService {
             String nickname = addUserRequest.getNickname();
             String isActive = addUserRequest.getIsActive();
 
-            /*
-            if (nickname != null && !nickname.isEmpty()) {
-                User userByNickname = userRepository.findByNickname(addUserRequest.getNickname());
-                if (userByNickname != null) {
-                    if (!userByNickname.getUserId().equals(userId)) {
-                        throw new IllegalArgumentException("Nickname already exists. Please choose another nickname");
-                    }
-                }
+            if (nickname != null) {
                 user.setNickname(nickname);
-            }
-            */
-
-            if (isNicknameValid(addUserRequest.getNickname(),userId)) {
-                user.setNickname(nickname);
-            } else {
-                throw new IllegalArgumentException("Nickname already exists. Please choose another nickname");
             }
 
             if (isActive != null && !isActive.isEmpty()) {
@@ -73,24 +58,7 @@ public class UserService {
         }
     }
 
-    public Optional<User> getUserByNickname(String nickname) {
+    public User getUserByNickname(String nickname) {
         return userRepository.findByNickname(nickname);
-    }
-
-    public boolean isNicknameValid(String nickname, Long userId) {
-        Optional<User> user = userRepository.findByNickname(nickname);
-        boolean isValid = true;
-
-        if (user != null) {
-            isValid = false;
-
-            //it is valid if it's used by the same user id
-            if (userId != null) {
-                if (user.get().equals(userId)) {
-                    isValid = true;
-                }
-            }
-        }
-        return isValid;
     }
 }
