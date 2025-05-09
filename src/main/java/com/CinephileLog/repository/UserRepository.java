0de46fd4182481a5f21d3dd6a682c6b24670ae20
ehdwo13 <ehdwo13@gmail.com>
2025-05-10 05:10:32 +0900

    feat(user): update point when grade reevaluation runs

--- src/main/java/com/CinephileLog/repository/UserRepository.java
+++ src/main/java/com/CinephileLog/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByNicknameContaining(String keyword);
     User findByNickname(String nickname);
     @Modifying
+    @Transactional
     @Query("UPDATE User u SET u.grade.gradeId = :gradeId WHERE u.userId = :userId")
     void updateGrade(Long userId, Long gradeId);
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.point = :point WHERE u.userId = :userId")
+    void updateUserPoint(Long userId, Long point);
 }
