44be4b8117625c2e1e6f326484f03f67619e4b27
elini_ng <huangenni@yahoo.com>
2025-05-09 18:14:25 +0900

    1. Remove role-info.html 2. Add grade introduction page (gradeInfo.html, gradeViewController.java and gradeInfo.css, and images) 3. Change image name from login_image.png to popcorn_image.png due to the usage from different pages (logIn.html, myProfile.html, and gradeInfo.html)

---
+++ src/main/java/com/CinephileLog/controller/GradeViewController.java
@@ -0,0 +1,81 @@
+package com.CinephileLog.controller;
+
+import com.CinephileLog.domain.User;
+import com.CinephileLog.service.GradeService;
+import com.CinephileLog.service.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+
+@Controller
+public class GradeViewController {
+    private final UserService userService;
+
+    public GradeViewController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/gradeInfo")
+    public String gradeDescView(@AuthenticationPrincipal OAuth2User user, Model model) {
+        //For header
+        User userInfo = userService.getUserById(user.getAttribute("userId"));
+        model.addAttribute("user", userInfo);
+        model.addAttribute("showMenu", true);
+
+        //For content
+        //List<Grade> grades = gradeService.getAllGrades();
+
+        List<Map<String, String>> gradeList = new ArrayList<>();
+        gradeList.add(createGrade("popcorn", "This is popcorn description"));
+        gradeList.add(createGrade("hotdog", "This is hotdog description"));
+        gradeList.add(createGrade("coke", "This is coke description"));
+        gradeList.add(createGrade("nachos", "This is nachos description"));
+        gradeList.add(createGrade("jelly", "This is jelly description"));
+
+        int centerX = 150 + 80;  //radius + roughly image width
+        int centerY = 150 + 80;  //radius + image height
+        int radiusX = 150;
+        int radiusY = 180;
+
+        List<Map<String, Object>> positionedGrades = new ArrayList<>();
+
+        for (int i = 0; i < gradeList.size() ; i++) {
+            Map<String, String> gradeMap = gradeList.get(i);
+
+            double angle = Math.PI / 2 + (Math.PI * i / (gradeList.size() - 1));  //calculate half circle From π/2 (top) to 3π/2 (bottom)
+            int x = (int) (Math.cos(angle) * radiusX + centerX);
+            int y = (int) (Math.sin(angle) * radiusY + centerY);
+
+            if (i == 0) {
+                y += 30;
+            }
+
+            int descX = centerX + 50;
+            int descY = centerY + 100;
+
+            //grade data put in by descending order of original sequence in objects because position is in clockwise rotation
+            Map<String, Object> map = new HashMap<>();
+            map.put("gradeName", gradeMap.get("gradeName"));
+            map.put("x", x);
+            map.put("y", y);
+            map.put("gradeDesc", gradeMap.get("gradeDesc"));
+            map.put("descX", descX);
+            map.put("descY", descY);
+            positionedGrades.add(map);
+        }
+
+        model.addAttribute("grades", positionedGrades);
+        return "gradeInfo";
+    }
+
+    private static Map<String, String> createGrade(String gradeName, String gradeDesc) {
+        Map<String, String> gradeMap = new HashMap<>();
+        gradeMap.put("gradeName", gradeName);
+        gradeMap.put("gradeDesc", gradeDesc);
+        return gradeMap;
+    }
+}
